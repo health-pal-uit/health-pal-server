ee2d249f37acfda3aa7e57e330d8630e85b3239d
Khong <hankhongg@gmail.com>
2025-10-16 00:56:06 +0700

    chore(setup): swagger on

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,6 +19,14 @@ async function bootstrap() {
     //optionsSuccessStatus: 204,
     credentials: true,
   });
+  const config = new DocumentBuilder()
+    .setTitle('Health Pal')
+    .setDescription('The Health Pal API description')
+    .setVersion('1.0')
+    .addTag('health-pal')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3001);
 }
