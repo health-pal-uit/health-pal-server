c33d5fea053bab62eef51a93c5b7c605902c6fad
Khong <hankhongg@gmail.com>
2025-10-16 01:11:38 +0700

    chore(config): add Joi validation for database environment variables

---
+++ src/config/configuration.ts
@@ -0,0 +1,9 @@
+export default () => ({
+  DB_DATABASE: 'health-pal-db',
+  DB_USERNAME: 'postgres',
+  DB_PORT: 5432,
+  DB_PASSWORD: '135792468',
+  DB_HOST: 'localhost',
+});
+
+// same w env for simplicity
