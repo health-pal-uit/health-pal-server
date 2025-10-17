13ffecf043e8d4ce40129bb83fcfa0e5eb8e2fe9
Khong <hankhongg@gmail.com>
2025-10-17 17:50:01 +0700

    feat(crud): daily_logs, daily_meals, daily_ingredients

--- src/daily_ingres/daily_ingres.module.ts
+++ src/daily_ingres/daily_ingres.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { DailyIngresService } from './daily_ingres.service';
 import { DailyIngresController } from './daily_ingres.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DailyIngre } from './entities/daily_ingre.entity';
+import { DailyLog } from 'src/daily_logs/entities/daily_log.entity';
+import { Ingredient } from 'src/ingredients/entities/ingredient.entity';
+import { DailyLogsModule } from 'src/daily_logs/daily_logs.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([DailyIngre, DailyLog, Ingredient]), DailyLogsModule],
   controllers: [DailyIngresController],
   providers: [DailyIngresService],
 })
